9bda38ef8ba15e99336ce9405043d51f9267b03a
Casdegraaff <54799365+Casdegraaff@users.noreply.github.com>
2020-10-01 13:02:44 +0200

    added Arduino

---
+++ Arduino/sketch_oct01a/sketch_oct01a.ino
@@ -0,0 +1,94 @@
+int led1 = 13; // give it a name:
+int led2 = 12;
+int led3 = 11;
+int led4 = 10;
+int led5 = 9;
+int led6 = 8;
+int led7 = 7;
+int led8 = 6;
+int led9 = 5;
+int led10 = 4;
+int led11 = 3;
+void setup() {          
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+  pinMode(led7, OUTPUT);
+  pinMode(led8, OUTPUT);
+  pinMode(led9, OUTPUT);
+  pinMode(led10, OUTPUT);
+  pinMode(led11, OUTPUT);      
+}
+ 
+void loop() {
+    digitalWrite(led1, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led1, LOW);  // turn the LED off
+delay(100);            // wait for a second
+
+digitalWrite(led2, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led2, LOW);  // turn the LED off
+delay(100);            // wait for a second
+
+digitalWrite(led3, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led3, LOW);  // turn the LED off
+delay(100);            // wait for a second
+
+digitalWrite(led4, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led4, LOW);  // turn the LED off
+delay(100);            // wait for a second
+
+digitalWrite(led5, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led5, LOW);  // turn the LED off
+delay(100);            // wait for a second
+
+digitalWrite(led6, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led6, LOW);  // turn the LED off
+delay(100);  // wait for a second
+
+digitalWrite(led7, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led7, LOW);  // turn the LED off
+delay(100);
+
+digitalWrite(led8, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led8, LOW);  // turn the LED off
+delay(100);
+
+digitalWrite(led9, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led9, LOW);  // turn the LED off
+delay(100);
+
+digitalWrite(led10, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led10, LOW);  // turn the LED off
+delay(100);
+
+digitalWrite(led11, HIGH);   // turn the LED on
+delay(100);            // wait for a second
+ 
+    digitalWrite(led11, LOW);  // turn the LED off
+delay(100);
+
+
+}
